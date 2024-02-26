1d09806703206c1250687380a3536e47dadceafd
Albert Ragothi Wiecaszek <wiecaszekalbert@gmail.com>
2024-02-26 21:15:21 +0100

    first issue

---
+++ AvaloniaApplication1/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace AvaloniaApplication1;
+
+public class User{
+    public int id { get; set; }
+    public string username { get; set; }
+    public bool adult { get; set; }
+
+    public User(int id, string username, bool adult){
+        this.id = id;
+        this.username = username;
+        this.adult = adult;
+    }
+}
